5623ff914ceb728288c45eac723b95964236f192
dnksebastian <sebastian.dulniawka@gmail.com>
2023-02-20 16:26:01 +0100

    results summary component

--- results-summary-component-main/main.js
+++ results-summary-component-main/main.js
@@ -1,1 +1,63 @@
-console.log(".");
+// DOM Elements & global variables
+
+const resultElement = document.getElementById('result');
+
+const detailsResultListEl = document.getElementById('details-list');
+
+const scoresArr = [];
+
+// Functions
+
+ async function getScoreData() {
+     await fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(obj => {
+            renderDataEl(obj, detailsResultListEl);
+            scoresArr.push(+obj.score);
+        });
+        resultElement.textContent = getScoreAvg(scoresArr);
+      })
+      .catch(error => console.log(error))
+}
+
+
+function renderDataEl(dataInput, container) {
+    const liEl = document.createElement('li');
+    const liIconWrap = document.createElement('div');
+    const iconImg = document.createElement('img');
+    const skillName = document.createElement('span');
+    const scoreHelper = document.createElement('div');
+    const detailScore = document.createElement('span');
+    const detScoreHelp = document.createElement('span');
+
+    liEl.classList.add('detail-li');
+    liIconWrap.classList.add('icon-wrap');
+    iconImg.classList.add('icon');
+    skillName.classList.add('skill-name');
+    scoreHelper.classList.add('detail');
+    scoreHelper.classList.add('score-helper');
+    detailScore.classList.add('detail-score');
+    detScoreHelp.textContent = '/ 100';
+
+    liEl.appendChild(liIconWrap)
+    liEl.appendChild(skillName)
+    liEl.appendChild(scoreHelper)
+    liIconWrap.appendChild(iconImg);
+    scoreHelper.appendChild(detailScore);
+    scoreHelper.appendChild(detScoreHelp);
+
+    skillName.textContent = dataInput.category;
+    detailScore.textContent = dataInput.score;
+    iconImg.src = dataInput.icon;
+
+    container.appendChild(liEl);
+};
+
+function getScoreAvg(arr) {
+    const sum = arr.reduce((a, b) => a + b, 0);
+    const avg = Math.round((sum / arr.length));
+    return avg
+};
+
+getScoreData();
